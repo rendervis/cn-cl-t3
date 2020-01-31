e5b45a097cd3ee8943a172c664a17be3caa78ae1
renderVis <rendervis333@gmail.com>
2020-01-31 10:22:33 +0200

    homepage to smaller components, styling menu-item component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+
+import HomePage from "./pages/home-page/home-page.component";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
