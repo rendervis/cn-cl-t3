b7d87b297366e76a0154a453bcc4cd70f4378a93
renderVis <rendervis333@gmail.com>
2020-02-02 11:50:51 +0200

    added header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Switch, Route } from "react-router-dom";
 
-import logo from "./logo.svg";
+import Header from "./components/header/header.component";
 
 import HomePage from "./pages/home-page/home-page.component";
 import ShopPage from "./pages/shop-page/shop-page.component";
@@ -12,6 +12,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
