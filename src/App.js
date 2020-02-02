e1c76125042f36bbaa4d9aa01685ccc2fa32e76e
renderVis <rendervis333@gmail.com>
2020-02-02 09:46:31 +0200

    updating shop page, collection preview component and collection item

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 
 import HomePage from "./pages/home-page/home-page.component";
+import ShopPage from "./pages/shop-page/shop-page.component";
 
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
